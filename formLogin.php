e5da52149712f608f9758e5bd6a3803967367652
Cedric-M <cirdec95@gmail.com>
2017-04-12 10:29:30 +0100

    Ajout du formulaire Login

---
+++ formLogin.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+
+Bienvenu ! Votre pseudo est : <?php echo $_POST["pseudo"]; ?><br>
+Votre password est: <?php echo $_POST["password"]; ?>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -2,55 +2,61 @@
 <html>
 
 <head>
-    <meta type="content" charset="utf-8" />
-    <title>Connexion</title>
+	<meta type="content" charset="utf-8" />
+	<title>Connexion</title>
 
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <!-- CSS --><!-- 
   <link rel="stylesheet" href="assets/css/main.css" />  -->
 </head>
 
 <body id="body">
 	<!-- Form -->
-		<section id="form">
 
 
-			<div class="container">
-				<h3>Se connecter:</h3>
-				<form>
-					<div class="row">
-						<div class="col-sm-6">
-							<div class="form-group">
-								<!-- <label for="nom">Nom:</label> -->
-								<input type="nom" class="form-control" id="nom" placeholder="Pseudo">
-							</div>
-						</div>
-						<div class="col-sm-6">
-							<div class="form-group">
-								<!-- <label for="email">Email:</label> -->
-								<input type="email" class="form-control" id="email" placeholder="Password" >
-							</div>
-						</div>
+	<section id="form">
+
+
+		<div class="container">
+			<form action="formLogin.php" method="post">
+				<!-- Name:  <input type="text" name="pseudo"><br>
+				E-mail: <input type="text" name="password"><br>
+				<input type="submit"> -->
+			
+
+			<div class="row">
+				<div class="col-sm-6">
+					<div class="form-group">
+						<!-- <label for="nom">Nom:</label> -->
+						<input type="text" class="form-control" placeholder="Pseudo" name="pseudo">
 					</div>
-					
-
-					<button type="submit" class="btn btn-primary" onclick="Login();">Login</button>
-
-					<button class="btn btn-default" onclick="newAccount();" type="button">
-     New Account</button>
-
+				</div>
+				<div class="col-sm-6">
+					<div class="form-group">
+						<!-- <label for="email">Email:</label> -->
+						<input type="text" class="form-control" placeholder="Password" name="password" >
+					</div>
+				</div>
+			</div>
+			<button type="submit" class="btn btn-primary" onclick="document.getElementById('body').style.backgroundColor='green';">Login</button>
+			<button class="btn btn-default" onclick="newAccount();" type="button">
+				New Account</button>
 </form>
 
-				</form>
+			
+
+			
+
+
 			</div>
 
 		</section>
-    
 
 
-<!-- jQuery & JS -->
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-<script type="text/javascript" src="assets/js/javascript.js"></script>
 
-</body>
-</html>
+		<!-- jQuery & JS -->
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		<script type="text/javascript" src="assets/js/javascript.js"></script>
+
+	</body>
+	</html>
